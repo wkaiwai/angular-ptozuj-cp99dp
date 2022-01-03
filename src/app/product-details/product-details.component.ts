bcf720b5003c283b4118d337a67ac794ed0d8dce
wkaiwai <gordon.wkw91@gmail.com>
2022-01-03 23:14:05 +0800

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,7 +17,11 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private cartService: CartService
     ) { }
-  
+
+    addToCart(product: Product) {
+      this.cartService.addToCart(product);
+      window.alert('Your product has been added to the cart!');
+    }  
 
   ngOnInit(){
     // First get the product id from the current route.
@@ -27,7 +31,5 @@ export class ProductDetailsComponent implements OnInit {
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
   }
-
   
-
 }
